57b1a3665f9bdb98452b07054c802c4b0afca827
teapotsoup <0902ab@naver.com>
2022-03-15 10:26:05 +0900

    first commit
    tsc-watch 적용

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHi  = (name:string, age:number, gender:string): string=>{
+    return `Hello ${name}, you are ${age}, you are ${gender}`;
+}
+console.log(sayHi("seo", 25, "male"));
+export{};
